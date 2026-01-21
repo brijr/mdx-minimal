30a79bcce137a44c44d2b61877e83bba472f8624
Bridger Tower <bridgertower@gmail.com>
2026-01-20 21:02:03 -0700

    update hero

--- components/site/hero.tsx
+++ components/site/hero.tsx
@@ -2,14 +2,11 @@ import { Section, Container } from "@/components/ds";
 
 export const Hero = () => {
   return (
-    <Section className="border-b">
+    <Section>
       <Container>
-        <h1 className="text-2xl font-semibold tracking-tight mb-2">MDX Starter</h1>
+        <h1 className="">brijr/web-starter</h1>
         <p className="text-muted-foreground">
-          A minimal MDX starter with Next.js and Tailwind.{" "}
-          <a href="https://github.com/brijr/mdx" className="underline underline-offset-4">
-            View on GitHub
-          </a>
+          A minimal typescript website starter with next.js, velite, tailwind, and shadcn/ui.{" "}
         </p>
       </Container>
     </Section>
