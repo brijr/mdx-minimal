3b4c6891456e67ddb0b7f0ef37fec9179b203c9a
Bridger Tower <bridgertower@gmail.com>
2026-01-20 21:41:20 -0700

    wip

--- components/posts/item.tsx
+++ components/posts/item.tsx
@@ -11,7 +11,9 @@ export const Item = ({ slug, title, date }: PostItemProps) => (
   <li>
     <Link href={`/${slug}`} className="block group">
       <div className="flex justify-between items-baseline gap-3">
-        <h3 className="min-w-0 flex-1">{title}</h3>
+        <div className="min-w-0 flex-1 flex gap-1">
+          <h3 className="group-hover:underline group-hover:underline-offset-4 decoration-muted-foreground decoration-dotted">{title}</h3>
+        </div>
         <time dateTime={date} className="text-muted-foreground shrink-0">
           {formatDate(date)}
         </time>
--- components/posts/list.tsx
+++ components/posts/list.tsx
@@ -7,7 +7,7 @@ export const List = ({ posts }: { posts: Post[] }) => {
   return (
     <Section>
       <Container className="space-y-2">
-        <h2>Posts</h2>
+        <h2 className="text-muted-foreground">Posts</h2>
         {posts.length > 0 ? (
           <ul className="grid gap-1.5" >
             {posts.map((post) => (
